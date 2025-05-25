22543a28afb389e22bde0856e335ed57285a6aab
Christopher Fandrich <cgfcubing@gmail.com>
2025-05-24 22:57:25 -0700

    mm: Add documentation for vmm functions

--- include/crescent/mm/vmm.h
+++ include/crescent/mm/vmm.h
@@ -24,10 +24,57 @@ struct vmm_ctx {
 	pte_t* pagetable;
 };
 
+/**
+ * @brief Map I/O memory to the virtual address space
+ *
+ * @param physical The physical address to map
+ * @param size The size of the mapping
+ * @param mmu_flags The protection flags
+ *
+ * @return NULL on failure, otherwise you get an address in the iomem vma
+ */
 void __iomem* iomap(physaddr_t physical, size_t size, unsigned long mmu_flags);
+
+/**
+ * @brief Unmap I/O memory
+ *
+ * @param virtual The virtual address to unmap
+ * @param size The size of the mapping
+ *
+ * @return 0 on success, -errno on failure
+ */
 int iounmap(void __iomem* virtual, size_t size);
 
+/**
+ * @brief Map some memory into the virtual address space
+ *
+ * @param mm_flags The MM flags for the memory allocations
+ * @param size The size of the mapping
+ * @param mmu_flags The protection flags to use for the mapping
+ *
+ * @return NULL on failure, otherwise you get an address in the kernel vma
+ */
 void* kmap(mm_t mm_flags, size_t size, unsigned long mmu_flags);
+
+/**
+ * @brief Change the MMU flags of a page(s)
+ *
+ * @param virtual The virtual address
+ * @param size The size of the mapping
+ * @param mmu_flags The new MMU flags
+ *
+ * @return 0 on success, -errno on failure
+ */
 int kprotect(void* virtual, size_t size, unsigned long mmu_flags);
+
+/**
+ * @brief Unmap virtual memory pages
+ *
+ * @param virtual The virtual address to unmap
+ * @param size The size of the mapping
+ *
+ * @return 0 on success, -errno on failure
+ */
 int kunmap(void* virtual, size_t size);
+
 void vmm_init(void);
