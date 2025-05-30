35dd8a853c915c7449243400383a1d03a35ed100
Christopher Fandrich <cgfcubing@gmail.com>
2025-05-29 22:01:06 -0700

    core: use primitive types for interrupt context

--- include/crescent/core/interrupt.h
+++ include/crescent/core/interrupt.h
@@ -1,32 +1,16 @@
 #pragma once
 
-#include <crescent/types.h>
-
 struct ctx {
 	void* cr2;
-	u64 rax;
-	u64 rbx;
-	u64 rcx;
-	u64 rdx;
-	u64 rsi;
-	u64 rdi;
+	long rax, rbx, rcx, rdx, rsi, rdi;
 	void* rbp;
-	u64 r8;
-	u64 r9;
-	u64 r10;
-	u64 r11;
-	u64 r12;
-	u64 r13;
-	u64 r14;
-	u64 r15;
-	const void* __isr_common_ret;
-	u64 int_num;
-	u64 err_code;
+	long r8, r9, r10, r11, r12, r13, r14, r15;
+	const void* __asm_isr_common_ret;
+	long int_num, err_code;
 	void* rip;
-	u64 cs;
-	u64 rflags;
+	long cs, rflags;
 	void* rsp;
-	u64 ss;
+	long ss;
 } __attribute__((packed));
 
 void interrupts_init(void);
