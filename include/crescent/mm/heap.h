6a6bc7f0e56907a0a405f9460cc4b36c688eeb47
Christopher Fandrich <cgfcubing@gmail.com>
2025-05-30 23:13:38 -0700

    mm: add null check to kzalloc

--- include/crescent/mm/heap.h
+++ include/crescent/mm/heap.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <crescent/common.h>
 #include <crescent/mm/mm.h>
 
 void* kmalloc(size_t size, mm_t mm_flags);
@@ -8,6 +9,8 @@ void kfree(void* addr);
 
 static inline void* kzalloc(size_t size, mm_t mm_flags) {
 	void* ret = kmalloc(size, mm_flags);
+	if (!ret)
+		return NULL;
 	__builtin_memset(ret, 0, size);
 	return ret;
 }
