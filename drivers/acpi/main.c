7a800f63dd7d0bcd658501bc9a819dccb14fa2c8
Christopher Fandrich <cgfcubing@gmail.com>
2025-06-06 00:08:00 -0700

    acpi: small init changes

--- drivers/acpi/main.c
+++ drivers/acpi/main.c
@@ -1,15 +1,20 @@
+#include <crescent/compiler.h>
 #include <crescent/asm/errno.h>
 #include <crescent/core/module.h>
 #include <crescent/mm/heap.h>
 #include <acpi/madt.h>
 #include <uacpi/uacpi.h>
+#include <uacpi/context.h>
 
 static int acpi_init(void) {
+	uacpi_context_set_log_level(UACPI_LOG_DEBUG);
+	uacpi_context_set_proactive_table_checksum(UACPI_TRUE);
+
 	void* early_tables = kmalloc(PAGE_SIZE, MM_ZONE_NORMAL);
 	if (!early_tables)
 		return -ENOMEM;
 	uacpi_status err = uacpi_setup_early_table_access(early_tables, PAGE_SIZE);
-	if (err != UACPI_STATUS_OK) {
+	if (unlikely(err != UACPI_STATUS_OK)) {
 		kfree(early_tables);
 		return -ENODATA;
 	}
