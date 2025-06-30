aaa8baf383d06934be04cf6b3172f2264cc702c4
Christopher Fandrich <cgfcubing@gmail.com>
2025-06-29 22:15:33 -0700

    core: Add assert macro

--- include/crescent/core/panic.h
+++ include/crescent/core/panic.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <crescent/compiler.h>
+
 _Noreturn void panic(const char* fmt, ...);
+
+#define assert(c) \
+	do { \
+		if (unlikely(!(c))) \
+			panic("%s:%d: %s failed!", __FILE__, __LINE__, #c); \
+	} while (0)
