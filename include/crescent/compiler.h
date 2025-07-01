fc317ecf3320f57d8d8502d5865fb6a860757194
Christopher Fandrich <cgfcubing@gmail.com>
2025-06-30 17:40:39 -0700

    Add diagnostic pragma macros

--- include/crescent/compiler.h
+++ include/crescent/compiler.h
@@ -1,13 +1,23 @@
 #pragma once
 
-#ifdef CONFIG_LLVM
-#define __iomem volatile __attribute__((noderef))
-#else
-#define __iomem volatile
-#endif /* CONFIG_LLVM */
-
 #define __asmlinkage __attribute__((sysv_abi))
 #define __noinline __attribute__((noinline))
 
 #define likely(c) __builtin_expect(!!(c), 1)
 #define unlikely(c) __builtin_expect(!!(c), 0)
+
+#define __do_pragma(x) _Pragma(#x)
+
+#ifdef CONFIG_LLVM
+#define __iomem volatile __attribute__((noderef))
+
+#define __diag_push() __do_pragma(clang diagnostic push)
+#define __diag_ignore(w) __do_pragma(clang diagnostic ignored w)
+#define __diag_pop() __do_pragma(clang diagnostic pop)
+#else /* CONFIG_LLVM */
+#define __iomem volatile
+
+#define __diag_push() __do_pragma(GCC diagnostic push)
+#define __diag_ignore(w) __do_pragma(GCC diagnostic ignored w)
+#define __diag_pop() __do_pragma(GCC diagnostic pop)
+#endif /* CONFIG_LLVM */
