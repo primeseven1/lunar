9d55e60a59a12bbfbfa8f342799e7c983c2023e0
Christopher Fandrich <cgfcubing@gmail.com>
2025-07-08 21:33:21 -0700

    sched: Remove i8253 calibration, use timekeeper

--- kernel/sched/timer.c
+++ kernel/sched/timer.c
@@ -2,43 +2,11 @@
 #include <crescent/common.h>
 #include <crescent/core/cpu.h>
 #include <crescent/core/io.h>
+#include <crescent/core/timekeeper.h>
 #include <crescent/core/apic.h>
 #include <crescent/core/printk.h>
 #include <crescent/core/panic.h>
 #include "sched.h"
-
-#define I8253_FREQ 1193182
-#define I8253_CHANNEL0 0x40
-#define I8253_COMMAND 0x43
-#define I8253_MAX_MS 54
-
-#define I8253_WAIT_LENGTH 10u
-
-static void __i8253_wait(u8 ms) {
-	u16 count = (I8253_FREQ / 1000) * ms;
-
-	outb(I8253_COMMAND, 0x30);
-	outb(I8253_CHANNEL0, count & 0xFF);
-	outb(I8253_CHANNEL0, count >> 8);
-
-	u16 current;
-	do {
-		u8 low = inb(I8253_CHANNEL0);
-		u8 high = inb(I8253_CHANNEL0);
-		current = (high << 8) | low;
-	} while (current);
-}
-
-
-static void i8253_wait(unsigned long ms) {
-	while (ms > I8253_MAX_MS) {
-		__i8253_wait(I8253_MAX_MS);
-		ms -= I8253_MAX_MS;
-	}
-
-	if (ms)
-		__i8253_wait(ms);
-}
 
 static void timer(const struct isr* isr, struct context* ctx) {
 	(void)isr;
@@ -46,20 +14,18 @@ static void timer(const struct isr* isr, struct context* ctx) {
 	sched_switch(ctx);
 }
 
-static spinlock_t i8253_lock = SPINLOCK_STATIC_INITIALIZER;
-
 static struct irq timer_irq = {
 	.irq = -1,
 	.eoi = apic_eoi
 };
 
+#define USEC_CALIBRATION_TIME 10000ul
+
 void sched_timer_init(void) {
-	spinlock_lock(&i8253_lock);
 	lapic_write(LAPIC_REG_TIMER_DIVIDE, 0x03);
 	lapic_write(LAPIC_REG_TIMER_INITIAL, U32_MAX);
-	i8253_wait(I8253_WAIT_LENGTH);
+	timekeeper_stall(USEC_CALIBRATION_TIME);
 	lapic_write(LAPIC_REG_LVT_TIMER, 1 << 16);
-	spinlock_unlock(&i8253_lock);
 
 	u32 ticks = U32_MAX - lapic_read(LAPIC_REG_TIMER_CURRENT);
 
@@ -70,6 +36,6 @@ void sched_timer_init(void) {
 	lapic_write(LAPIC_REG_TIMER_DIVIDE, 0x03);
 	lapic_write(LAPIC_REG_TIMER_INITIAL, ticks);
 
-	printk(PRINTK_DBG "sched: LAPIC timer calibrated at %u ticks per %u ms on CPU %u\n", 
-			ticks, I8253_WAIT_LENGTH, current_cpu()->processor_id);
+	printk(PRINTK_DBG "sched: LAPIC timer calibrated at %u ticks per %lu us on CPU %u\n", 
+			ticks, USEC_CALIBRATION_TIME, current_cpu()->processor_id);
 }
