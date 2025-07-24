b8bc10abcbcc1fa40060bb622ff88d5297c14656
Christopher Fandrich <cgfcubing@gmail.com>
2025-07-24 13:59:34 -0700

    print errors after term_init(), use kthread_exit at the end of kernel_main

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -16,6 +16,7 @@
 #include <crescent/mm/heap.h>
 #include <crescent/asm/segment.h>
 #include <crescent/sched/sched.h>
+#include <crescent/sched/kthread.h>
 
 #include <acpi/acpi_init.h>
 
@@ -29,19 +30,15 @@ __diag_ignore("-Wmissing-prototypes");
 
 _Noreturn __asmlinkage void kernel_main(void) {
 	int base_revision = limine_base_revision();
-	if (base_revision != LIMINE_BASE_REVISION)
-		goto die;
+	if (base_revision != LIMINE_BASE_REVISION) {
+		while (1)
+			__asm__ volatile("hlt");
+	}
 
 	bsp_cpu_init();
 
-	/* Attempt to load early modules */
-	int err = module_load("e9hack");
-	if (err && err != -ENOENT)
-		printk(PRINTK_WARN "init: e9hack module init failed (is this real hardware?) err %i\n", err);
-
-	err = tracing_init();
-	if (err)
-		printk(PRINTK_WARN "init: tracing_init failed with code %i\n", err);
+	int err_e9hack = module_load("e9hack"); /* Enable early debugging */
+	int err_trace = tracing_init(); /* Enable stack traces */
 
 	buddy_init();
 	vmm_init();
@@ -49,41 +46,45 @@ _Noreturn __asmlinkage void kernel_main(void) {
 	interrupts_init();
 	heap_init();
 
-	err = cmdline_parse();
-	if (err)
-		printk("init: Failed to parse cmdline! err: %i\n", err);
-
-	const char* loglevel = cmdline_get("loglevel");
-	if (loglevel) {
-		unsigned int level = *loglevel - '0';
-		err = printk_set_level(level);
-		if (err == -EINVAL)
-			printk(PRINTK_WARN "init: Failed to set cmdline loglevel, invalid level: %u", level);
-		else if (err)
-			printk(PRINTK_WARN "init: Failed to set cmdline loglevel, err: %i", err);
-	}
-
 	init_status = INIT_STATUS_MM;
 
-	term_init();
+	/* now we can use a generic error variable, since this is the last error before a term is initialized */
+	int err = cmdline_parse();
+
+	term_init(); /* Allow the user to actually see errors */
+	if (unlikely(err)) {
+		printk(PRINTK_ERR "init: Failed to parse cmdline! err: %i\n", err);
+	} else {
+		const char* loglevel = cmdline_get("loglevel");
+		if (loglevel) {
+			unsigned int level = *loglevel - '0';
+			err = printk_set_level(level);
+			if (err == -EINVAL)
+				printk(PRINTK_WARN "init: Failed to set cmdline loglevel, invalid level: %u", level);
+			else if (err)
+				printk(PRINTK_WARN "init: Failed to set cmdline loglevel, err: %i", err);
+		}
+	}
+
+	if (err_e9hack && err_e9hack != -ENOENT)
+		printk(PRINTK_WARN "init: e9hack module init failed (is this real hardware?) err %i\n", err_e9hack);
+	if (unlikely(err_trace))
+		printk(PRINTK_WARN "init: tracing_init failed with code %i\n", err_trace);
 
 	err = acpi_init();
-	if (err)
+	if (unlikely(err))
 		panic("ACPI failed to initialize, err: %i", err);
+
 	err = apic_bsp_init();
-	if (err)
+	if (unlikely(err))
 		panic("Failed to initialize APIC, err: %i", err);
-
 	timekeeper_init();
-
-	sched_init();
+	sched_init(); /* After this, this thread can now be treated as a kthread */
 	init_status = INIT_STATUS_SCHED;
 
-	printk(PRINTK_CRIT "init: kernel_main ended!\n");
+	printk(PRINTK_CRIT "init: kernel_main thread ended!\n");
 	local_irq_enable();
-die:
-	while (1)
-		cpu_halt();
+	kthread_exit(NULL);
 }
 
 __diag_pop();
