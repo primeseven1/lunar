4d8421c2f755d65871e6b1b81f3341bf9f068c07
Christopher Fandrich <cgfcubing@gmail.com>
2025-07-29 00:24:48 -0700

    add sparse macros

--- include/crescent/compiler.h
+++ include/crescent/compiler.h
@@ -6,18 +6,12 @@
 #define likely(c) __builtin_expect(!!(c), 1)
 #define unlikely(c) __builtin_expect(!!(c), 0)
 
+/* Used by sparse */
+#define __user __attribute__((noderef, address_space(1)))
+#define __iomem __attribute__((noderef, address_space(2)))
+#define __force __attribute__((force))
+
 #define __do_pragma(x) _Pragma(#x)
-
-#ifdef CONFIG_LLVM
-#define __iomem volatile __attribute__((noderef))
-
-#define __diag_push() __do_pragma(clang diagnostic push)
-#define __diag_ignore(w) __do_pragma(clang diagnostic ignored w)
-#define __diag_pop() __do_pragma(clang diagnostic pop)
-#else /* CONFIG_LLVM */
-#define __iomem volatile
-
 #define __diag_push() __do_pragma(GCC diagnostic push)
 #define __diag_ignore(w) __do_pragma(GCC diagnostic ignored w)
 #define __diag_pop() __do_pragma(GCC diagnostic pop)
-#endif /* CONFIG_LLVM */
