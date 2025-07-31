507d26f9731dd152a095a538cc6261ac3658e3e3
Christopher Fandrich <cgfcubing@gmail.com>
2025-07-31 01:57:35 -0700

    core: add spinlock_init

--- include/crescent/core/locking.h
+++ include/crescent/core/locking.h
@@ -4,8 +4,7 @@
 
 typedef volatile atomic(unsigned long) spinlock_t;
 
-#define SPINLOCK_INITIALIZER 0
-#define SPINLOCK_STATIC_INITIALIZER atomic_static_init(SPINLOCK_INITIALIZER)
+#define SPINLOCK_INITIALIZER atomic_static_init(0)
 
 void spinlock_lock(spinlock_t* lock);
 void spinlock_unlock(spinlock_t* lock);
@@ -13,3 +12,7 @@ bool spinlock_try(spinlock_t* lock);
 void spinlock_lock_irq_save(spinlock_t* lock, unsigned long* flags);
 void spinlock_unlock_irq_restore(spinlock_t* lock, unsigned long* flags);
 bool spinlock_try_irq_save(spinlock_t* lock, unsigned long* flags);
+
+static inline void spinlock_init(spinlock_t* lock) {
+	atomic_store(lock, 0, ATOMIC_RELAXED);
+}
