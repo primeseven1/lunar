6f5ca834a17fd58f73c0c6816ad645b45367131a
Christopher Fandrich <cgfcubing@gmail.com>
2025-08-19 00:23:43 -0700

    core: update for scheduler changes

--- include/crescent/core/semaphore.h
+++ include/crescent/core/semaphore.h
@@ -22,7 +22,7 @@ static inline void semaphore_init(struct semaphore* sem, long count) {
 	spinlock_init(&sem->lock);
 }
 
-void semaphore_wait(struct semaphore* sem);
+int semaphore_wait(struct semaphore* sem);
 int semaphore_wait_timed(struct semaphore* sem, time_t timeout_ms);
 void semaphore_signal(struct semaphore* sem);
 bool semaphore_try(struct semaphore* sem);
