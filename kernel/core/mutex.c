d167f5d56f3bbc7ad35ef3692637033ddf9903e4
Christopher Fandrich <cgfcubing@gmail.com>
2025-09-03 23:39:39 -0700

    core: Use init status instead of a current_thread check in mutex

--- kernel/core/mutex.c
+++ kernel/core/mutex.c
@@ -1,31 +1,29 @@
 #include <crescent/core/mutex.h>
 #include <crescent/core/panic.h>
 #include <crescent/core/printk.h>
+#include <crescent/init/status.h>
 
 void mutex_lock(mutex_t* lock) {
-	struct thread* current_thread = current_cpu()->runqueue.current;
-	if (unlikely(!current_thread)) {
+	if (unlikely(init_status_get() < INIT_STATUS_SCHED)) {
 		spinlock_lock(&lock->spinlock);
 		return;
 	}
 
-	if (unlikely(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread))
-		panic("mutex_lock deadlock");
+	struct thread* current_thread = current_cpu()->runqueue.current;
+	bug(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread);
+
 	semaphore_wait(&lock->sem, 0);
 	atomic_store(&lock->owner, current_thread, ATOMIC_RELEASE);
 }
 
 int mutex_lock_timed(mutex_t* lock, time_t timeout_ms) {
-	struct thread* current_thread = current_cpu()->runqueue.current;
-	if (unlikely(!current_thread)) {
+	if (unlikely(init_status_get() < INIT_STATUS_SCHED)) {
 		spinlock_lock(&lock->spinlock);
 		return 0;
 	}
 
-	if (unlikely(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread)) {
-		printk(PRINTK_CRIT "core: thread already has lock, guaranteed timeout (%s)\n", __func__);
-		return -ETIMEDOUT;
-	}
+	struct thread* current_thread = current_cpu()->runqueue.current;
+	bug(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread);
 
 	int res = semaphore_wait_timed(&lock->sem, timeout_ms, 0);
 	if (res == 0)
@@ -34,14 +32,11 @@ int mutex_lock_timed(mutex_t* lock, time_t timeout_ms) {
 }
 
 bool mutex_try_lock(mutex_t* lock) {
-	struct thread* current_thread = current_cpu()->runqueue.current;
-	if (unlikely(!current_thread))
+	if (unlikely(init_status_get() < INIT_STATUS_SCHED))
 		return spinlock_try(&lock->spinlock);
 
-	if (unlikely(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread)) {
-		printk(PRINTK_CRIT "core: thread already has lock, guaranteed fail (%s)\n", __func__);
-		return false;
-	}
+	struct thread* current_thread = current_cpu()->runqueue.current;
+	bug(atomic_load(&lock->owner, ATOMIC_ACQUIRE) == current_thread);
 
 	bool success = semaphore_try(&lock->sem);
 	if (success)
@@ -50,14 +45,13 @@ bool mutex_try_lock(mutex_t* lock) {
 }
 
 void mutex_unlock(mutex_t* lock) {
-	struct thread* current_thread = current_cpu()->runqueue.current;
-	if (unlikely(!current_thread)) {
+	if (unlikely(init_status_get() < INIT_STATUS_SCHED)) {
 		spinlock_unlock(&lock->spinlock);
 		return;
 	}
 
-	if (unlikely(atomic_load(&lock->owner, ATOMIC_ACQUIRE) != current_thread))
-		panic("mutex_unlock not owner");
+	struct thread* current_thread = current_cpu()->runqueue.current;
+	bug(atomic_load(&lock->owner, ATOMIC_ACQUIRE) != current_thread);
 
 	atomic_store(&lock->owner, NULL, ATOMIC_RELEASE);
 	semaphore_signal(&lock->sem);
