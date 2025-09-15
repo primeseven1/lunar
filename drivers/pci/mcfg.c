9b3f648756c73de3723101a1f22e7bfc86cbc23d
Christopher Fandrich <cgfcubing@gmail.com>
2025-09-15 01:11:48 -0700

    pci: Map whole MCFG segment

--- drivers/pci/mcfg.c
+++ drivers/pci/mcfg.c
@@ -10,24 +10,32 @@
 
 #include "internal.h"
 
-static const struct acpi_mcfg_allocation* mcfg_entries = NULL;
-static size_t mcfg_entry_count = 0;
+struct ecam_seg {
+	physaddr_t physical;
+	size_t len;
+	void __iomem* virtual;
+	u8 start_bus;
+	u8 end_bus;
+};
+
+static struct ecam_seg* ecam_segs;
+static size_t ecam_seg_count;
 
 static inline u32 __iomem* pci_address(const struct pci_device* device, u32 func, u32 off) {
 	uintptr_t byte_off = ((uintptr_t)func << 12) | (off & 0xFFC);
 	return (u32 __iomem*)((uintptr_t)device->virtual + byte_off);
 }
 
-static const struct acpi_mcfg_allocation* get_mcfg_entry(u8 bus) {
-	const struct acpi_mcfg_allocation* entry;
+static const struct ecam_seg* get_ecam_entry(u8 bus) {
+	const struct ecam_seg* entry;
 	size_t i;
-	for (i = 0; i < mcfg_entry_count; i++) {
-		entry = &mcfg_entries[i];
+	for (i = 0; i < ecam_seg_count; i++) {
+		entry = &ecam_segs[i];
 		if (bus >= entry->start_bus && bus <= entry->end_bus)
 			break;
 	}
 
-	if (i == mcfg_entry_count)
+	if (i == ecam_seg_count)
 		return NULL;
 
 	return entry;
@@ -40,7 +48,7 @@ static inline bool pci_mcfg_args_ok(u32 func, u32 off) {
 u32 pci_mcfg_read(struct pci_device* device, u32 func, u32 off) {
 	if (!pci_mcfg_args_ok(func, off))
 		return U32_MAX;
-	const struct acpi_mcfg_allocation* entry = get_mcfg_entry(device->bus);
+	const struct ecam_seg* entry = get_ecam_entry(device->bus);
 	if (!entry)
 		return U32_MAX;
 
@@ -51,7 +59,7 @@ u32 pci_mcfg_read(struct pci_device* device, u32 func, u32 off) {
 int pci_mcfg_write(struct pci_device* device, u32 func, u32 off, u32 value) {
 	if (!pci_mcfg_args_ok(func, off))
 		return -EINVAL;
-	const struct acpi_mcfg_allocation* entry = get_mcfg_entry(device->bus);
+	const struct ecam_seg* entry = get_ecam_entry(device->bus);
 	if (!entry)
 		return -ENODEV;
 
@@ -61,12 +69,12 @@ int pci_mcfg_write(struct pci_device* device, u32 func, u32 off, u32 value) {
 }
 
 #define PCI_ECAM_FUNC_SIZE 4096ul
-#define PCI_ECAM_DEV_SIZE (PCI_ECAM_FUNC_SIZE * PCI_MAX_FUNC)
+#define PCI_ECAM_DEV_SIZE  (PCI_ECAM_FUNC_SIZE * PCI_MAX_FUNC)
+#define PCI_ECAM_BUS_SIZE (PCI_ECAM_FUNC_SIZE * PCI_MAX_FUNC * PCI_MAX_DEV)
 
-static inline size_t ecam_device_offset(const struct acpi_mcfg_allocation* entry, u32 bus, u32 dev) {
+static inline size_t ecam_device_offset(const struct ecam_seg* entry, u32 bus, u32 dev) {
 	size_t bus_index = bus - entry->start_bus;
-	size_t device_index = dev;
-	return (bus_index * PCI_MAX_DEV + device_index) * PCI_ECAM_DEV_SIZE;
+	return (bus_index * PCI_MAX_DEV + dev) * PCI_ECAM_DEV_SIZE;
 }
 
 int pci_mcfg_device_open(u32 bus, u32 dev, struct pci_device** out) {
@@ -74,25 +82,21 @@ int pci_mcfg_device_open(u32 bus, u32 dev, struct pci_device** out) {
 	if (bus >= PCI_MAX_BUS || dev >= PCI_MAX_DEV)
 		return -EINVAL;
 
-	const struct acpi_mcfg_allocation* entry = get_mcfg_entry(bus);
+	const struct ecam_seg* entry = get_ecam_entry(bus);
 	if (!entry)
 		return -ENODEV;
+
 	struct pci_device* device = kmalloc(sizeof(*device), MM_ZONE_NORMAL);
 	if (!device)
 		return -ENOMEM;
 
-	physaddr_t device_physical = entry->address + ecam_device_offset(entry, bus, dev);
-	device->virtual = iomap(device_physical, PCI_ECAM_DEV_SIZE, MMU_READ | MMU_WRITE);
-	if (!device->virtual) {
-		kfree(device);
-		return -ENOMEM;
-	}
+	uintptr_t off = ecam_device_offset(entry, bus, dev);
+	device->virtual = (void __iomem*)((uintptr_t)entry->virtual + off);
 	device->bus = bus;
 	device->dev = dev;
 
 	u16 vendor = pci_read_config_word(device, 0, PCI_CONFIG_VENDOR);
 	if (vendor == 0xFFFF || vendor == 0) {
-		bug(iounmap(device->virtual, PCI_ECAM_DEV_SIZE) != 0);
 		kfree(device);
 		return -ENODEV;
 	}
@@ -102,13 +106,6 @@ int pci_mcfg_device_open(u32 bus, u32 dev, struct pci_device** out) {
 }
 
 int pci_mcfg_device_close(struct pci_device* device) {
-	const struct acpi_mcfg_allocation* entry = get_mcfg_entry(device->bus);
-	if (!entry)
-		return -EINVAL;
-	int err = iounmap(device->virtual, PCI_ECAM_DEV_SIZE);
-	if (err)
-		return err;
-
 	kfree(device);
 	return 0;
 }
@@ -120,8 +117,43 @@ int pci_mcfg_init(void) {
 		return -ENOSYS;
 
 	struct acpi_mcfg* mcfg = table.ptr;
-	mcfg_entries = mcfg->entries;
-	mcfg_entry_count = (mcfg->hdr.length - sizeof(*mcfg)) / sizeof(struct acpi_mcfg_allocation);
+	const struct acpi_mcfg_allocation* mcfg_entries = mcfg->entries;
+	size_t mcfg_entry_count = (mcfg->hdr.length - sizeof(*mcfg)) / sizeof(struct acpi_mcfg_allocation);
 
-	return 0;
+	int err = 0;
+	if (unlikely(mcfg_entry_count == 0)) {
+		err = -ENOSYS; /* ? */
+		goto cleanup;
+	}
+
+	ecam_segs = kzalloc(sizeof(*ecam_segs) * mcfg_entry_count, MM_ZONE_NORMAL);
+	if (!ecam_segs) {
+		err = -ENOMEM;
+		goto cleanup;
+	}
+
+	for (ecam_seg_count = 0; ecam_seg_count < mcfg_entry_count; ecam_seg_count++) {
+		physaddr_t physical = mcfg_entries[ecam_seg_count].address;
+		size_t len = (mcfg_entries[ecam_seg_count].end_bus - mcfg_entries[ecam_seg_count].start_bus + 1) * PCI_ECAM_BUS_SIZE;
+		void __iomem* virtual = iomap(physical, len, MMU_READ | MMU_WRITE);
+		if (!virtual) {
+			err = -EIO;
+			goto cleanup;
+		}
+
+		ecam_segs[ecam_seg_count].physical = physical;
+		ecam_segs[ecam_seg_count].virtual = virtual;
+		ecam_segs[ecam_seg_count].len = len;
+		ecam_segs[ecam_seg_count].start_bus = mcfg_entries[ecam_seg_count].start_bus;
+		ecam_segs[ecam_seg_count].end_bus = mcfg_entries[ecam_seg_count].end_bus;
+	}
+
+cleanup:
+	uacpi_table_unref(&table);
+	if (err && ecam_segs) {
+		for (size_t i = 0; i < ecam_seg_count; i++)
+			iounmap(ecam_segs[i].virtual, ecam_segs[i].len);
+		kfree(ecam_segs);
+	}
+	return err;
 }
