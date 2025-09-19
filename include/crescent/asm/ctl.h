4ae4f04ae4ddeb1d285b501017ed97eb39c506a8
Christopher Fandrich <cgfcubing@gmail.com>
2025-09-19 00:20:47 -0700

    asm: Add doxygen comments for control registers

--- include/crescent/asm/ctl.h
+++ include/crescent/asm/ctl.h
@@ -39,38 +39,66 @@
 
 #include <crescent/types.h>
 
+/**
+ * @brief Read from the CR0 flags register
+ * @return The value of the register
+ */
 static inline unsigned long ctl0_read(void) {
 	unsigned long ret;
 	__asm__("mov %%cr0, %0" : "=r"(ret));
 	return ret;
 }
 
+/**
+ * @brief Write to the CR0 flags register
+ * @return The new value of the register
+ */
 static inline void ctl0_write(unsigned long flags) {
 	__asm__ volatile("mov %0, %%cr0" : : "r"(flags) : "memory");
 }
 
+/**
+ * @brief Read the page fault address
+ * @return The address that caused the fault
+ */
 static inline void* ctl2_read(void) {
 	void* ret;
 	__asm__("mov %%cr2, %0" : "=r"(ret) : : "memory");
 	return ret;
 }
 
+/**
+ * @brief Read the top level page table address
+ * @return The top level page table
+ */
 static inline physaddr_t ctl3_read(void) {
 	unsigned long ret;
 	__asm__("mov %%cr3, %0" : "=r"(ret) : : "memory");
 	return (physaddr_t)ret;
 }
 
+/**
+ * @brief Set the CPU's top level page table
+ * @param pagetable The physical address of the page table
+ */
 static inline void ctl3_write(physaddr_t pagetable) {
 	__asm__ volatile("mov %0, %%cr3" : : "r"((unsigned long)pagetable) : "memory");
 }
 
+/**
+ * @brief Read from the CR4 flags register
+ * @return The value of the register
+ */
 static inline unsigned long ctl4_read(void) {
 	unsigned long ret;
 	__asm__("mov %%cr4, %0" : "=r"(ret));
 	return ret;
 }
 
+/**
+ * @brief Write to the CR4 flags register
+ * @param flags The new flags to write
+ */
 static inline void ctl4_write(unsigned long flags) {
 	__asm__ volatile("mov %0, %%cr4" : : "r"(flags) : "memory");
 }
