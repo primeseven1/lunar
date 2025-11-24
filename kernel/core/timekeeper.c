8bb41d9790bdfb7c2cbc65ab368a76a7b9df76f0
Christopher Fandrich <cgfcubing@gmail.com>
2025-11-23 23:24:46 -0800

    core: Better logic in timekeeper + bug fixes

--- kernel/core/timekeeper.c
+++ kernel/core/timekeeper.c
@@ -7,30 +7,6 @@
 #include <lunar/core/cpu.h>
 #include <lunar/core/interrupt.h>
 
-extern struct timekeeper _ld_kernel_timekeepers_start[];
-extern struct timekeeper _ld_kernel_timekeepers_end[];
-
-static struct timekeeper* get_timekeeper(int type, bool early) {
-	struct timekeeper* start = _ld_kernel_timekeepers_start;
-	struct timekeeper* end = _ld_kernel_timekeepers_end;
-
-	unsigned long count = ((uintptr_t)end - (uintptr_t)start) / sizeof(*start);
-	struct timekeeper* best = NULL;
-	for (unsigned long i = 0; i < count; i++) {
-		if (start[i].type != type)
-			continue;
-		if (start[i].rating == 0 || start[i].early != early)
-			continue;
-
-		if (!best)
-			best = &start[i];
-		else if (start[i].rating > best->rating)
-			best = &start[i];
-	}
-
-	return best;
-}
-
 static inline time_t scale_ticks_to_ns(time_t ticks, unsigned long long freq) {
 	i128 tmp = (i128)ticks * 1000000000ull;
 	return tmp / freq;
@@ -39,23 +15,24 @@ static inline time_t scale_ticks_to_ns(time_t ticks, unsigned long long freq) {
 static struct timekeeper_source* wallclock_source = NULL;
 
 struct timespec timekeeper_time(int type) {
-	irqflags_t irq = local_irq_save();
-
-	struct timekeeper_source* timekeeper;
+	struct timekeeper_source* tk_src;
 	struct timespec ts = { .tv_sec = 0, .tv_nsec = 0 };
+
+	irqflags_t irq = local_irq_save();
 
 	switch (type) {
 	case TIMEKEEPER_FROMBOOT: {
-		timekeeper = current_cpu()->timekeeper;
-		time_t ticks = timekeeper ? ticks = timekeeper->fb_ticks() : 0;
-		time_t nsec = ticks ? scale_ticks_to_ns(ticks, timekeeper->fb_freq) : 0;
+		tk_src = current_cpu()->timekeeper;
+		time_t ticks = tk_src ? ticks = tk_src->fb_ticks() : 0;
+		time_t nsec = ticks ? scale_ticks_to_ns(ticks, tk_src->fb_freq) : 0;
 		ts.tv_sec = nsec / 1000000000ull;
 		ts.tv_nsec = nsec % 1000000000ull;
 		break;
 	}
 	case TIMEKEEPER_WALLCLOCK: {
-		timekeeper = wallclock_source;
-		ts = timekeeper->wc_get(timekeeper);
+		tk_src = wallclock_source;
+		if (tk_src)
+			ts = tk_src->wc_get(tk_src);
 		break;
 	}
 	default:
@@ -68,28 +45,55 @@ struct timespec timekeeper_time(int type) {
 
 void timekeeper_stall(time_t usec) {
 	irqflags_t irq_state = local_irq_save();
+	struct timekeeper_source* tk_src = current_cpu()->timekeeper;
 
-	struct timekeeper_source* timekeeper = current_cpu()->timekeeper;
-	bug(timekeeper == NULL); /* Trying to stall with no timekeeper when expecting a real stall can be bad */
-	bug(usec > 5000000); /* More than 5 seconds is dumb */
+	/*
+	 * Returning instead of a panic can be bad since a stall is required for some hardware,
+	 * and stalling for more than 5 seconds is dumb
+	 */
+	bug(tk_src == NULL);
+	bug(usec > 5000000);
 
-	unsigned long long ticks_per_us = timekeeper->fb_freq / 1000000;
-	time_t start = timekeeper->fb_ticks();
+	unsigned long long ticks_per_us = tk_src->fb_freq / 1000000;
+	time_t start = tk_src->fb_ticks();
 	time_t end = start + (usec * ticks_per_us);
 
-	while (timekeeper->fb_ticks() < end)
+	while (tk_src->fb_ticks() < end)
 		cpu_relax();
 
 	local_irq_restore(irq_state);
 }
 
-static struct timekeeper* get_timekeeper_init(int type, bool early, struct timekeeper_source** out) {
-	struct timekeeper* keeper = get_timekeeper(type, early);
+extern struct timekeeper _ld_kernel_timekeepers_start[];
+extern struct timekeeper _ld_kernel_timekeepers_end[];
+
+static struct timekeeper* __get_timekeeper(int type, bool early) {
+	struct timekeeper* best = NULL;
+	size_t count = _ld_kernel_timekeepers_end - _ld_kernel_timekeepers_start;
+
+	for (size_t i = 0; i < count; i++) {
+		struct timekeeper* tk = &_ld_kernel_timekeepers_start[i];
+		if (tk->type != type)
+			continue;
+		if (tk->rating == 0 || tk->early != early)
+			continue;
+
+		if (!best)
+			best = tk;
+		else if (tk->rating > best->rating)
+			best = tk;
+	}
+
+	return best;
+}
+
+static struct timekeeper* get_timekeeper(int type, bool early, struct timekeeper_source** out_src) {
+	struct timekeeper* keeper = __get_timekeeper(type, early);
 	while (keeper) {
-		if (keeper->init(out) == 0)
+		if (keeper->init(out_src) == 0)
 			break;
 		keeper->rating = 0;
-		keeper = get_timekeeper(0, early);
+		keeper = __get_timekeeper(type, early);
 	}
 	return keeper;
 }
@@ -114,14 +118,13 @@ void timekeeper_init(void) {
 
 	/* The real timekeeper may need another working timekeeper */
 	struct timekeeper_source* source;
-	early_keeper = get_timekeeper_init(TIMEKEEPER_FROMBOOT, true, &source);
+	early_keeper = get_timekeeper(TIMEKEEPER_FROMBOOT, true, &source);
 	if (!early_keeper)
 		panic("No early timekeeper could be selected");
 	current_cpu()->timekeeper = source;
 	name = early_keeper->name;
 
-	/* Now initialize a real timekeeper (might just use the early one) */
-	struct timekeeper* real = get_timekeeper_init(TIMEKEEPER_FROMBOOT, false, &source);
+	struct timekeeper* real = get_timekeeper(TIMEKEEPER_FROMBOOT, false, &source);
 	if (real) {
 		name = real->name;
 		keeper = real;
@@ -130,9 +133,8 @@ void timekeeper_init(void) {
 		keeper = early_keeper;
 	}
 
-	if (!get_timekeeper_init(TIMEKEEPER_WALLCLOCK, false, &wallclock_source))
-		panic("No wallclock source!\n");
+	get_timekeeper(TIMEKEEPER_WALLCLOCK, false, &wallclock_source);
 
-	atomic_thread_fence(ATOMIC_SEQ_CST); /* keeper is read by other CPU's after this call, so do a fence here */
+	atomic_thread_fence(ATOMIC_RELEASE);
 	printk(PRINTK_INFO "core: Timekeeper source %s chosen (frequency %llu mhz)\n", name, source->fb_freq / 1000000);
 }
