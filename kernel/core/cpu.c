23214af7abf53d3bbb9ca03bc039479c1d52abf2
Christopher Fandrich <cgfcubing@gmail.com>
2025-12-05 00:54:01 -0800

    core: Combine code back into the cpu.c file (again)

--- kernel/core/cpu.c
+++ kernel/core/cpu.c
@@ -1,15 +1,18 @@
 #include <lunar/asm/wrap.h>
+#include <lunar/asm/cpuid.h>
+#include <lunar/asm/segment.h>
 #include <lunar/core/cpu.h>
 #include <lunar/core/printk.h>
+#include <lunar/core/syscall.h>
 #include <lunar/core/interrupt.h>
 #include <lunar/core/timekeeper.h>
 #include <lunar/core/apic.h>
+#include <lunar/lib/string.h>
 #include <lunar/mm/buddy.h>
 #include <lunar/mm/hhdm.h>
 #include <lunar/init/head.h>
-#include "internal.h"
 
-struct limine_mp_request __limine_request g_mp_request = {
+static struct limine_mp_request __limine_request mp_request = {
 	.request.id = LIMINE_MP_REQUEST,
 	.request.revision = 0,
 	.response = NULL
@@ -59,10 +62,10 @@ void smp_send_stop(void) {
 }
 
 void smp_struct_init(void) {
-	size_t size = sizeof(struct smp_cpus) + (g_mp_request.response->cpu_count * sizeof(struct cpu*));
+	size_t size = sizeof(struct smp_cpus) + (mp_request.response->cpu_count * sizeof(struct cpu*));
 	physaddr_t address = alloc_pages(MM_ZONE_NORMAL | MM_NOFAIL, get_order(size));
 	smp_cpus = hhdm_virtual(address);
-	smp_cpus->count = g_mp_request.response->cpu_count;
+	smp_cpus->count = mp_request.response->cpu_count;
 }
 
 void smp_cpu_init_finish(void) {
@@ -80,7 +83,7 @@ void smp_startup(void) {
 			panic("smp_startup() failed: interrupt_register(): %i", err);
 	}
 
-	struct limine_mp_response* mp = g_mp_request.response;
+	struct limine_mp_response* mp = mp_request.response;
 	atomic_store(&cpus_left, mp->cpu_count - 1);
 	for (u64 i = 0; i < mp->cpu_count; i++) {
 		if (mp->cpus[i]->lapic_id == mp->bsp_lapic_id)
@@ -95,4 +98,51 @@ void smp_startup(void) {
 
 	if (smp_cpus->count - 1)
 		printk(PRINTK_INFO "smp: Brought up %u CPUs\n", smp_cpus->count - 1);
+}
+
+static void enable_syscall(void) {
+	u32 _unused, edx;
+	cpuid(CPUID_EXT_LEAF_PROC_INFO, 0, &_unused, &_unused, &_unused, &edx);
+	if (unlikely(!(edx & (1 << 11))))
+		panic("syscall instruction unsupported by CPU");
+
+	wrmsr(MSR_EFER, rdmsr(MSR_EFER) | MSR_EFER_SCE);
+	wrmsr(MSR_LSTAR, (uintptr_t)asm_syscall_entry);
+	wrmsr(MSR_STAR, SEGMENT_KERNEL_CODE << 16 | SEGMENT_USER_CODE);
+	wrmsr(MSR_CSTAR, 0);
+	wrmsr(MSR_SF_MASK, CPU_FLAG_INTERRUPT);
+}
+
+static atomic(u32) sched_ids = atomic_init(1);
+
+void percpu_ap_init(struct limine_mp_info* mp_info) {
+	struct cpu* cpu = hhdm_virtual(alloc_pages(MM_ZONE_NORMAL | MM_NOFAIL, get_order(sizeof(*cpu))));
+	memset(cpu, 0, sizeof(*cpu));
+
+	cpu->self = cpu;
+	cpu->lapic_id = mp_info->lapic_id;
+	cpu->processor_id = mp_info->processor_id;
+	cpu->sched_processor_id = atomic_fetch_add(&sched_ids, 1);
+	wrmsr(MSR_GS_BASE, (uintptr_t)cpu);
+
+	enable_syscall();
+}
+
+void percpu_bsp_init(void) {
+	static struct cpu bsp_cpu = {
+		.self = &bsp_cpu,
+		.sched_processor_id = 0,
+	};
+
+	struct limine_mp_response* mp = mp_request.response;
+	bsp_cpu.lapic_id = mp->bsp_lapic_id;
+	for (u64 i = 0; i < mp->cpu_count; i++) {
+		if (mp->cpus[i]->lapic_id == mp->bsp_lapic_id) {
+			bsp_cpu.processor_id = mp->cpus[i]->processor_id;
+			break;
+		}
+	}
+	wrmsr(MSR_GS_BASE, (uintptr_t)&bsp_cpu);
+
+	enable_syscall();
 }
--- kernel/core/cpu/internal.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <lunar/core/limine.h>
-
-extern struct limine_mp_request g_mp_request;
