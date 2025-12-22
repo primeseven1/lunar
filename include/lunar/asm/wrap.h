7bc7ffad838288ef1becdee14fe0f304eccecdd7
Christopher Fandrich <cgfcubing@gmail.com>
2025-12-21 23:00:43 -0800

    asm: Add more wrappers

--- include/lunar/asm/wrap.h
+++ include/lunar/asm/wrap.h
@@ -1,4 +1,17 @@
 #pragma once
 
+#ifndef __ASSEMBLER__
+
+#include <lunar/types.h>
+
 #define cpu_relax() __asm__ volatile("pause" : : : "memory")
 #define cpu_halt() __asm__ volatile("hlt" : : : "memory")
+#define cpu_ldmxcsr(m) \
+	do { \
+		u32 __mxcsr = m; \
+		__asm__ volatile("ldmxcsr %0" : : "m"(__mxcsr) : "memory"); \
+	} while (0)
+#define cpu_fxsave(p) __asm__ volatile("fxsave (%0)" : : "r"(p) : "memory")
+#define cpu_fxrstor(p) __asm__ volatile("fxrstor (%0)" : : "r"(p) : "memory");
+
+#endif /* __ASSEMBLY_FILE__ */
