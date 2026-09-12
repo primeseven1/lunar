01a44d785dd8ddd3c36b4ca93f2b1bddf1e04c63
Christopher Fandrich <cgfcubing@gmail.com>
2026-09-11 20:44:21 -0700

    sys: Fix elf64_header_ok()

--- kernel/sys/elf.c
+++ kernel/sys/elf.c
@@ -35,12 +35,12 @@ bool elf64_header_ok(const struct elf64_ehdr* ehdr) {
 		return false;
 
 	if (ehdr->e_ident.class != ELF_CLASS_64 || ehdr->e_ident.data != ELF_DATA_2LSB ||
-			(ehdr->e_ident.version != ELF_EV_CURRENT && ehdr->e_version != ELF_EV_CURRENT) ||
+			ehdr->e_ident.version != ELF_EV_CURRENT || ehdr->e_version != ELF_EV_CURRENT ||
 			ehdr->e_ident.osabi != ELF_OSABI_SYSTEMV || ehdr->e_ident.osabi_version != 0)
 		return false;
 	if (ehdr->e_machine != ARCH_ELF_EM_ARCHITECTURE)
 		return false;
-	if (ehdr->e_type != ELF_ET_EXEC || ehdr->e_phentsize != sizeof(struct elf64_phdr) || ehdr->e_phnum == 0)
+	if ((ehdr->e_type != ELF_ET_EXEC && ehdr->e_type != ELF_ET_DYN) || ehdr->e_phentsize != sizeof(struct elf64_phdr) || ehdr->e_phnum == 0)
 		return false;
 
 	return true;
